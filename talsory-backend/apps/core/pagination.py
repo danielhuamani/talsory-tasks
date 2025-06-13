ca5773296438bcfe9cda37c0fd7aab34197118b1
daniel huamani <danielhuamani15@gmail.com>
2025-06-13 08:35:38 -0500

    updated pagination

--- talsory-backend/apps/core/pagination.py
+++ talsory-backend/apps/core/pagination.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 
 class StandardPagination(PageNumberPagination):
-    page_size = 1
+    page_size = 20
     max_page_size = 1000
     page_size_query_param = 'page_size'
 
